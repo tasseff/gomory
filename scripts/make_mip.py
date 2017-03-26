bf98294f4a8d17b8d9e59e8bfad47df366b12248
Byron Tasseff <byron@tasseff.com>
2017-03-26 17:21:21 -0400

    Updating make_mip.

--- scripts/make_mip.py
+++ scripts/make_mip.py
@@ -20,10 +20,10 @@ __status__ = "Development"
 def make_mip(num_constraints, num_variables, pure, output_path):
     # Create a feasible problem with constraints A' y <= c.
     # Start by creating a feasible "dual" (i.e., a standard-form primal).
-    A = np.random.randint(-5, 5, size = (num_variables, num_constraints))
-    x = np.random.randint(0, 5, size = (num_constraints, 1))
+    A = np.random.randint(-100, 100, size = (num_variables, num_constraints))
+    x = np.random.rand(num_constraints, 1)
     b = np.matmul(A, x)
-    c = np.random.randint(0, 5, size = (num_constraints, 1))
+    c = np.random.randint(0, 10000, size = (num_constraints, 1))
     A_T = A.transpose()
 
     model = grb.Model(os.path.basename(output_path))
